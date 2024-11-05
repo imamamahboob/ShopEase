ceb2886f8e41cbe6f56ce4c081ef2e980ed0030d
imamamahboob <imamamehboob27@gmail.com>
2024-11-06 01:40:22 +0500

    Initial commit of orders-management module of java

---
+++ order-service/src/main/java/com/example/orderservice/OrderController.java
@@ -0,0 +1,23 @@
+package com.example.orderservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+}
